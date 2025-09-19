62d79caadeddf729186abc3cd29be91982ebdaab
David <example@gmail.com>
2025-09-19 15:39:52 +0400

    first update

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let inc = 10,
+  decr = 10;
+
+// ++inc;
+// decr--;
+// console.log(inc++);
+// console.log(decr--);
+// console.log(inc);
+// console.log(5 % 2);
+console.log(inc === "decr");
